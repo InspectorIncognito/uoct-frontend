d3a0a4ce528a88c1124692db2cf24192a4a5f4ef
Mrtn-fa <mnfaray@gmail.com>
2024-06-23 17:59:38 -0400

    feat(Map): Added components & more
    Added SideInfoComponent for showing information inside the map
    Added 'croner' package to update the map every 15th minute.

---
+++ src/components/map/SpeedInfoComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import SideInfoComponent from "@/components/map/SideInfoComponent.vue";
+import ColorComponent from "@/components/map/ColorComponent.vue";
+
+interface ColorData {
+  color: string;
+  info: string;
+}
+
+defineProps<{ colorData: ColorData[]; top: number }>();
+</script>
+
+<template>
+  <SideInfoComponent :top="top">
+    <div class="speed-data" v-for="(obj, idx) in colorData" :key="idx">
+      <ColorComponent :color="obj.color" />
+      <div class="speed-label">{{ obj.info }}</div>
+    </div>
+  </SideInfoComponent>
+</template>
+
+<style scoped>
+.speed-data {
+  display: flex;
+  flex-direction: row;
+  padding-left: 16px;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  text-align: right;
+}
+.speed-label {
+  width: 180px;
+}
+</style>
